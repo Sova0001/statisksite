3ad5645556ec62811127a17246fefc1cb518bd44
Sofie <sova0001@stud.ek.dk>
2026-03-02 12:39:10 +0100

    js

---
+++ js/productlist.js
@@ -0,0 +1,37 @@
+const kategori = new URLSearchParams(window.location.search).get("category");
+
+const container = document.querySelector("main");
+
+const endpoint = `https://kea-alt-del.dk/t7/api/products?category=${kategori}`;
+
+function getData() {
+  fetch(endpoint)
+    .then((res) => res.json())
+    .then(showData);
+}
+
+function showData(json) {
+  let markup = "";
+
+  json.forEach((element) => {
+    console.log(element);
+    markup += ` <a href="product.html">
+        <article class="smallProduct onSale soldOut">
+          <img
+            src="https://kea-alt-del.dk/t7/images/webp/640/${element.id}.webp"
+            alt="product image"
+          />
+          <h3>${element.productdisplayname}</h3>
+          <p class="product_brand">Tshirts | Nike</p>
+          <p class="price">DKK <span>1299</span>,-</p>
+          <div class="discounted">
+            <p>Now DKK <span>${element.price}</span>,-</p>
+            <p><span>25</span>%</p>
+          </div>
+        </article>
+      </a>`;
+  });
+  container.innerHTML = markup;
+}
+
+getData();
